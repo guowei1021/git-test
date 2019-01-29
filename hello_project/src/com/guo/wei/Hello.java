3510d6bb3fafe4577438f928e1754b70b79c0c79
sunguowei <591673226@qq.com>
2019-01-29 22:17:49 +0800

    把本地工程添加到Git

---
+++ hello_project/src/com/guo/wei/Hello.java
@@ -0,0 +1,11 @@
+package com.guo.wei;
+
+/**
+ * @Author: sungw
+ * @Date: Create in 2019/1/29 22:06
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+    }
+}
