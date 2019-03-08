0460aac16aa1635ff23faca4b9d1ba883ac62398
sunguowei <591673226@qq.com>
2019-03-09 00:23:30 +0800

    提交数据

---
+++ xuecheng/xcEduservice/xc-service-api/src/main/java/com/xuecheng/api/cms/CmsConfigControllerApi.java
@@ -0,0 +1,16 @@
+package com.xuecheng.api.cms;
+
+import com.xuecheng.framework.domain.cms.CmsConfig;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * @Author: sungw
+ * @Date: Create in 2019/3/8 22:35
+ */
+@Api(value = "cms配置管理接口",description = "cms配置管理接口，提供数据模型的管理、查询接口")
+public interface CmsConfigControllerApi {
+
+    @ApiOperation("根据id查询cms配置信息")
+    public CmsConfig getModel(String id);
+}
